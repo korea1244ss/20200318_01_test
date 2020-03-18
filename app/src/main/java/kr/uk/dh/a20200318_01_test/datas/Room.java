5a7b814495cfc9b52c9f29144b433811fb6d9ced
korea1244ss <61854931+korea1244ss@users.noreply.github.com>
2020-03-18 14:51:27 +0900

    방데이터 클라스 생성

---
+++ app/src/main/java/kr/uk/dh/a20200318_01_test/datas/Room.java
@@ -0,0 +1,51 @@
+package kr.uk.dh.a20200318_01_test.datas;
+
+import java.io.Serializable;
+
+public class Room implements Serializable {
+
+    private int price;
+    private String address;
+    private int floor;
+    private String description;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Room(int price, String address, int floor, String description) {
+        this.price = price;
+        this.address = address;
+        this.floor = floor;
+        this.description = description;
+
+    }
+}
